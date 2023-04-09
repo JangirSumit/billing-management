33c60c89068b4ef3ebf6262548bd506486832e51
Sumit Jangir <sja@leapwork.com>
2023-04-09 11:39:48 +0530

    css fix

--- BillingManagement/wwwroot/scripts/script.js
+++ BillingManagement/wwwroot/scripts/script.js
@@ -47,5 +47,17 @@ function activateHeader() {
 }
 
 function renderFooter() {
+    const footerHtml = `
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+              <!--<li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>-->
+            </ul>
+        <p class="text-center text-body-secondary">© 2023, Jangir Technologies</p>
+    `;
+
+    document.querySelector("footer").innerHTML = footerHtml;
 
 }
