f1a22f6e3776ffd7eeb91124eb2f8e4ca9870cf6
Sumit Jangir <sja@leapwork.com>
2023-04-09 08:42:22 +0530

    Updata data and abstrations

---
+++ BillingManagement/Abstrations/IItemsManager.cs
@@ -0,0 +1,11 @@
+﻿namespace BillingManagement.Abstrations
+{
+    public interface IItemsManager
+    {
+        void GetRecords();
+        void GetRecord(Guid id);
+        void InsertRecord();
+        void DeleteRecord(Guid id);
+        void UpdateRecord(Guid id);
+    }
+}
---
+++ BillingManagement/Managers/ItemsManager.cs
@@ -0,0 +1,32 @@
+﻿using BillingManagement.Abstrations;
+
+namespace BillingManagement.Managers
+{
+    public class ItemsManager : IItemsManager
+    {
+        public void DeleteRecord(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetRecord(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertRecord()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRecord(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
