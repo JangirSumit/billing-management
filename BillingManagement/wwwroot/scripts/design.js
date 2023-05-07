2c8f13ec132002491846ff8522b6e15a61669e0a
Sumit Jangir <sja@leapwork.com>
2023-05-07 17:48:39 +0530

    fixx

--- BillingManagement/wwwroot/scripts/design.js
+++ BillingManagement/wwwroot/scripts/design.js
@@ -190,7 +190,7 @@ function getSeachSuggestionDataItem(dataItem, target) {
 
 function renderTextEditor() {
     tinymce.init({
-        selector: 'textarea',
+        selector: '#pdf-terms-conditions',
         menubar: '',
         plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
